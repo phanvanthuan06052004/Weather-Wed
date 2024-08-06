54ae2e13198e0d22f9d34f93bd38ecfd98201e9e
phanvanthuan06052004 <phanthuan652004@gmail.com>
2024-08-06 21:01:55 +0700

    update Readmi

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 const URLapi = "http://api.openweathermap.org/data/2.5/weather?q=";
 const apiKey = "";
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
